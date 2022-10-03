dd74801a36a5a715d9f21c42e96736b0c3d23887
aravk <aravkasi@gmail.com>
2022-10-03 10:43:21 -0500

    Remove unused scripts and update hibernate script

--- scripts/hibernate_critical.sh
+++ scripts/hibernate_critical.sh
@@ -3,7 +3,7 @@
 /usr/bin/acpi -b | /usr/bin/awk -F'[,:%]' '{print $2, $3}' | {
     read -r status capacity
 
-    if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
+    if [ "$status" = Discharging -a "$capacity" -lt 4 ]; then
         /usr/bin/systemctl hibernate
     fi
 }
